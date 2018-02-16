b9c2f65d607ee58871554c1f49ca033eead401e7
Tom Lendy <tlendy25@gmail.com>
2018-02-15 21:57:12 -0600

    adds python.py file to src code

---
+++ src/python.py
@@ -0,0 +1,12 @@
+# this is a comment in Python
+
+print(2+3)
+# this would print 5
+
+mckinney_texas_population = 180000
+print(mckinney_texas_population)
+# will print 180000
+mckinney_texas_population = mckinney_texas_population + 15000
+print(mckinney_texas_population)
+# will print 195000
+# similar to JS where you can take an existing variable and reassign a value
